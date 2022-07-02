6923e7cc596c13739de60ef7d41a78ff6e4bc122
FionaWekulo <fionawekulo+1@gmail.com>
2022-07-01 19:07:33 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-/*
- * main-Determing if n is positive negative or zero
+/**
+ * main-Entry point
  *
  * Description: Program will generate a random number for n
  *
