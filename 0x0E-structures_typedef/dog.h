580e220c5b3e0dd51daafc3c30e8ca2a6180dfa6
FionaWekulo <fionawekulo+1@gmail.com>
2022-08-01 03:36:09 -0700

    Added functions to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,12 @@ float age;
 char *owner;
 };
 typedef struct dog my_dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+my_dog *new_dog(char *name, float age, char *owner);
+
+void free_dog(my_dog *d);
+
 #endif /*DOG_H*/
